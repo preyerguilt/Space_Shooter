9e87e1087023151742f127a42e782c96cbbb53e5
preyerguilt <oscar.back96@outlook.com>
2019-01-01 14:22:47 +0100

    Fixed rock spawn function, added note to self aswell

--- Space_Shooter/Game1.cs
+++ Space_Shooter/Game1.cs
@@ -67,10 +67,7 @@ namespace SpaceShooterRevamped
                     Position = new Vector2(100,100),
                     Bullet = new Bullet(Content.Load<Texture2D>("Bob"))
                 },
-                new Rock(rockTexture) 
-                {
-                    Position = new Vector2(200,200)
-                }
+                new Rock(rockTexture)
             };
 
         }
--- Space_Shooter/Sprites/Rock.cs
+++ Space_Shooter/Sprites/Rock.cs
@@ -10,18 +10,18 @@ namespace SpaceShooterRevamped.Sprites
 {
     public class Rock : Sprite
     {
-
+        private Random random;
         public Rock(Texture2D texture)
             :base(texture)
         {
-            //Position = new Vector2(Game1.Random.Next(0, Game1.ScreenWidth-_texture.Width), 
-            //                       Game1.Random.Next(0, Game1.ScreenHeight));
+            random = new Random();
             LinearVelocity = Game1.Random.Next(3, 6);
-           
+            Position = SpawnPosition();
         }
 
         public override void Update(GameTime gameTime, List<Sprite> sprites)
         {
+            Position += Direction * LinearVelocity;
             foreach(var sprite in sprites)
             {
                 if (sprite == this) continue;
@@ -31,6 +31,30 @@ namespace SpaceShooterRevamped.Sprites
                     sprite.IsRemoved = true;
                 }
             }
+            if(Position.X < 0 && Position.X > Game1.ScreenWidth+1)
+                IsRemoved = true;
+
+            if(Position.Y < 0 && Position.Y > Game1.ScreenHeight+1)
+                IsRemoved = true;
+        }
+
+        
+        private Vector2 SpawnPosition(){
+            switch(random.Next(1,4)){
+                case 1:
+                    Direction = new Vector2(1,0);
+                    return new Vector2(0,random.Next(100,600));
+                case 2:
+                    Direction = new Vector2(-1,0);
+                    return new Vector2(1280,random.Next(100,600));
+                case 3:
+                    Direction = new Vector2(0,1);
+                    return new Vector2(random.Next(100,1200),0);
+                case 4:
+                    Direction = new Vector2(0,-1);
+                    return new Vector2(random.Next(100,1200),720);
+            }
+            return new Vector2(0,0);
         }
     }
 }
